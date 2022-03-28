831acb1b6660387acd5b59ce6140afc84b17467a
VkHyperNova <kiis.veikko@gmail.com>
2022-03-28 20:08:12 +0300

    4.Header Component JSX. 5. Installing React Icons. 6. Header Component CSS and Responsive design. 7. Floating Navbar. 8. About section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/mina.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+            <div className="about__me-image">
+              <img src={ME} alt="About Image" />
+            </div>
+        </div>
+        <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>3+ Years Coding</small>
+              </article>
+
+              <article className="about__card">
+                <FiUsers className='about__icon'/>
+                <h5>Clients</h5>
+                <small>3 Clients from Estonia</small>
+              </article>
+
+              <article className="about__card">
+                <VscFolderLibrary className='about__icon'/>
+                <h5>Projects</h5>
+                <small>10+ Completed Projects</small>
+              </article>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ducimus voluptatibus, consequatur eligendi molestias earum iure repellendus recusandae doloribus maxime esse quibusdam natus laudantium at, officiis est sequi amet tenetur?</p>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/mina.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Veikko Kiis</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
