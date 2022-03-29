db5eb8cec88b15bfdbacc1afb0b50ff696c9f0a2
VkHyperNova <kiis.veikko@gmail.com>
2022-03-29 18:32:18 +0300

    portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+
+/* data for the map */
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'Test Project 1',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  },
+]
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Porfolio</h2>
+
+      <div className="container portfolio__container">
+
+        {/* iterate over map */}
+       {
+         data.map(({id, image, title, github, demo}) => {
+           return (
+            <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className='btn'>Github</a>
+            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+          </article>
+           )
+         })
+       }
+
+      </div>
+
+    </section>
   )
 }
 
