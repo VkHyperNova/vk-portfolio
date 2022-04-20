c197998c267ebcac0349c8b29118ad8fba168ac1
VkHyperNova <kiis.veikko@gmail.com>
2022-04-20 13:21:37 +0300

    Send button shake effect

--- src/components/contact/SendButton.js
+++ src/components/contact/SendButton.js
@@ -2,23 +2,27 @@ import React, { useState } from 'react';
 
 const AnimatedButton = () => {
 
-    const [send, set_send] = useState(false);
+    const [send, setSend] = useState(false);
+    const [shake, setShake] = useState(false);
 
     const animate = () => {
 
         var name = document.getElementById('name').value;
         var email = document.getElementById('email').value;
         var message = document.getElementById('message').value;
-        
 
-        if (name.trim() != "", email != "", message != "") {
-            set_send(true);
-            setTimeout(() => set_send(false), 1000);
-        } 
+
+        if (name !== "", email !== "", message !== "") {
+            setSend(true);
+            setTimeout(() => setSend(false), 1000);
+        } else {
+            setShake(true);
+            setTimeout(() => setShake(false), 1000);
+        }
     }
 
     return (
-        <button onClick={animate} className={"btn btn-primary " + (send ? `send` : null)}>Send Message</button>
+        <button onClick={animate} className={"btn btn-primary " + (send ? `send` : null) + " " +  (shake ? `shake` : null)}>Send Message</button>
     );
 
 }
